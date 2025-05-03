3297d48467c9a740966d244c0c3b6d78b3bf0602
Karly KOUEKA <karlykoueka@hotmail.com>
2025-05-03 04:51:15 -0400

    Smallest C++ progream Updatte
    Add explanation

--- Ubuntu/000_SmallestCpp/src/small.cpp
+++ Ubuntu/000_SmallestCpp/src/small.cpp
@@ -1,3 +1,23 @@
 int main(void) {
     return 0;
 }
+
+/*
+Actually the smallest cpp program is 'int main() {}'
+
+'int' = return type of the function
+'main' = function name. This is the entry point of Every Cpp program
+'()' = reserved for function parameter
+'{}' = body of the function.
+The same way the parameters will be between the parentesis, the body of the function will be between cruly-braces.
+Within a block the instructions are executed sequentially.
+'int main() {}' or 'int main(void) {}' are practically the same 
+
+'int main() {}' is the decration of a function named 'main'. 
+It has a return type of 'int'. it doesn't take any parameter and has an empty body.
+
+*/
+
+//References
+//https://cplusplus.com/doc/tutorial/program_structure/
+//https://en.cppreference.com/w/cpp/language/main_function
