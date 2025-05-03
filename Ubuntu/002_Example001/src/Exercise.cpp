e77109d15ddddc34d007dfc9056170ae5fed603b
Karly KOUEKA <karlykoueka@hotmail.com>
2025-05-03 13:59:38 -0400

    Exercice update
    Add new case for exercice case

--- Ubuntu/002_Example001/src/Exercise.cpp
+++ Ubuntu/002_Example001/src/Exercise.cpp
@@ -43,8 +43,109 @@ int main(void) {
     std::cout << "\n  *****************";
 
     std::cout << "\n\n";
+    std::cout << "*** Multiplication Table (X5) ***\n";
+    std::cout << "-----------------\n";
+
+    std::cout << "\n0 x 5 = 0";
+    std::cout << "\n1 x 5 = 5";
+    std::cout << "\n2 x 5 = 10";
+    std::cout << "\n3 x 5 = 15";
+    std::cout << "\n4 x 5 = 20";
+    std::cout << "\n5 x 5 = 25";
+    std::cout << "\n6 x 5 = 30";
+    std::cout << "\n7 x 5 = 35";
+    std::cout << "\n8 x 5 = 40";
+    std::cout << "\n9 x 5 = 45";
+    std::cout << "\n10 x 5 = 50";
+    std::cout << "\n11 x 5 = 55";
+    std::cout << "\n12 x 5 = 60";
+
+    std::cout << "\n\n";
+    std::cout << "*** Conjugaison Present (Chanter) ***\n";
+    std::cout << "-----------------\n";
+
+    std::cout << "\nJe chante";
+    std::cout << "\nTu chantes";
+    std::cout << "\nIl/Elle chante";
+    std::cout << "\nNous chantons";
+    std::cout << "\nVous chantez";
+    std::cout << "\nIls/Elles chantent";
+
+    std::cout << "\n\n";
 
 
     return 0;
 }
 
+//Result
+/*
+
+*** Rectangle ***
+-----------------
+
+*----------+----------*
+|                     |
+|                     |
+|                     |
+*----------+----------*
+
+
+
+*** Square ***
+-----------------
+
+*----------+----------*
+|                     |
+|                     |
+|                     |
+|                     |
+|                     |
+|                     |
+|                     |
+*----------+----------*
+
+
+
+*** Filled Triangle ***
+-----------------
+
+          *
+         ***
+        *****
+       *******
+      *********
+     ***********
+    *************
+   ***************
+  *****************
+
+*** Multiplication Table (X5) ***
+-----------------
+
+0 x 5 = 0
+1 x 5 = 5
+2 x 5 = 10
+3 x 5 = 15
+4 x 5 = 20
+5 x 5 = 25
+6 x 5 = 30
+7 x 5 = 35
+8 x 5 = 40
+9 x 5 = 45
+10 x 5 = 50
+11 x 5 = 55
+12 x 5 = 60
+
+*** Conjugaison Present (Chanter) ***
+-----------------
+
+Je chante
+Tu chantes
+Il/Elle chante
+Nous chantons
+Vous chantez
+Ils/Elles chantent
+
+[1] + Done                       "/usr/bin/gdb" --interpreter=mi --tty=${DbgTerm} 0<"/tmp/Microsoft-MIEngine-In-nx2ued2b.btd" 1>"/tmp/Microsoft-MIEngine-Out-1cubaa4b.lud"
+
+*/
